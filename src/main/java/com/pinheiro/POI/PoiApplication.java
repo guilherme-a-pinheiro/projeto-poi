c4ea38be7c267806e1779d9944e7e2198e1635f7
Guilherme Alves Pinheiro <guiapinheiro29@gmail.com>
2024-05-21 21:47:03 -0300

    initial commit

---
+++ src/main/java/com/pinheiro/POI/PoiApplication.java
@@ -0,0 +1,13 @@
+package com.pinheiro.POI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PoiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pinheiro/POI/PoiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pinheiro.POI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PoiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
