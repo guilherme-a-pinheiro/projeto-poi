4a79a55e0f71910b66657edbb1a39456f2868df8
Guilherme Alves Pinheiro <guiapinheiro29@gmail.com>
2024-05-21 21:51:37 -0300

    POIRepository criado

---
+++ src/main/java/com/pinheiro/POI/repository/PointOfInterestRepository.java
@@ -0,0 +1,8 @@
+package com.pinheiro.POI.repository;
+
+import com.pinheiro.POI.entity.PointOfInterest;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PointOfInterestRepository extends JpaRepository<PointOfInterest, Long> {
+    
+}
