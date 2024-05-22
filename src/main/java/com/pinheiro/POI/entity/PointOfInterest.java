e78a8658279e53c087f8d50d3dc86709a45e937f
Guilherme Alves Pinheiro <guiapinheiro29@gmail.com>
2024-05-21 21:50:51 -0300

    Entity POI criada

---
+++ src/main/java/com/pinheiro/POI/entity/PointOfInterest.java
@@ -0,0 +1,23 @@
+package com.pinheiro.POI.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Table(name = "tb_pois")
+@Entity(name = "tb_pois")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class PointOfInterest {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private Long x;
+    private Long y;
+}
